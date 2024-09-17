9ecb8d2d3e6c6d84a4af2f66e92d7177c20a5f9c
muhammedraouf1992 <muhammedraouf92@gmail.com>
2024-09-17 15:35:10 +0300

    fixed some lcp problems

--- app/(frontend)/_components/HeroSection.jsx
+++ app/(frontend)/_components/HeroSection.jsx
@@ -45,6 +45,7 @@ const HeroSection = () => {
             fill
             sizes="(min-width: 1480px) 1336px, (min-width: 480px) calc(95.92vw - 64px), (min-width: 440px) calc(200vw - 524px), (min-width: 400px) calc(200vw - 484px), calc(100vw - 64px)"
             alt="swiper image"
+            priority
           />
         </SwiperSlide>
         <SwiperSlide>
